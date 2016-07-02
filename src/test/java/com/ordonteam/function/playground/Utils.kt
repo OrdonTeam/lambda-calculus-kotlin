57feee48dc3158efa68cbcad7ce80093b61ee19c
Karol Kowalski <ordon1313@gmail.com>
2016-07-02 15:37:46 +0200

    asBoolean

--- src/test/java/com/ordonteam/function/playground/Utils.kt
+++ src/test/java/com/ordonteam/function/playground/Utils.kt
@@ -27,6 +27,17 @@ class UtilsTest {
         assertEquals(6, 6.asFunction().asNumber())
         assertEquals(9, 9.asFunction().asNumber())
     }
+
+    @Test
+    fun shouldConvertToBoolean() {
+        assertEquals(true, TRUE.asBoolean())
+        assertEquals(false, FALSE.asBoolean())
+    }
+
+    @Test(expected = Throwable::class)
+    fun shouldConvertToBooleanThrowWhenNotBoolean() {
+        NOT.asBoolean()
+    }
 }
 
 fun Int.asFunction() = (1..this).fold(ZERO, { value, i -> NEXT.call(value) })
@@ -36,3 +47,23 @@ fun Boolean.asFunction() = if (this) TRUE else FALSE
 fun F.isNumber(value: Int) = asNumber() == value
 
 fun F.asNumber() = (this.call(Counter()).call(Count(0)) as Count).count
+
+fun F.asBoolean(): Boolean {
+    return if (isBehavingLikeTrue(this)) {
+        true
+    } else if (isBehavingLikeFalse(this)) {
+        false
+    } else {
+        throw RuntimeException("It is not a boolean")
+    }
+}
+
+fun isBehavingLikeTrue(hopefullyTrue: F): Boolean {
+    val expected = F { x -> x }
+    return expected == hopefullyTrue.call(expected).call({ x -> expected })
+}
+
+fun isBehavingLikeFalse(hopefullyFalse: F): Boolean {
+    val expected = F { x -> x }
+    return expected == hopefullyFalse.call({ x -> expected }).call(expected)
+}
