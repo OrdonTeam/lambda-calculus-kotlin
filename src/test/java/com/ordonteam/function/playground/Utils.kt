eaac3de65fdf0e7e6e359ba9d7fda319d1f13384
Karol Kowalski <ordon1313@gmail.com>
2016-07-02 14:19:57 +0200

    boolean as function

--- src/test/java/com/ordonteam/function/playground/Utils.kt
+++ src/test/java/com/ordonteam/function/playground/Utils.kt
@@ -8,6 +8,14 @@ class UtilsTest {
         assertFunctionRepresent(6, 6.asFunction())
         assertFunctionRepresent(100, 100.asFunction())
     }
+
+    @Test
+    fun shouldReturnFunctionalRepresentationOfBoolean() {
+        assertBehaveLikeTrue(true.asFunction())
+        assertBehaveLikeFalse(false.asFunction())
+    }
 }
 
 fun Int.asFunction() = (1..this).fold(ZERO, { value, i -> NEXT.call(value) })
+
+fun Boolean.asFunction() = if (this) TRUE else FALSE
