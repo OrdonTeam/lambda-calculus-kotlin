5b015890c453234fc061af6e96199a2d282139ae
Karol Kowalski <ordon1313@gmail.com>
2016-07-02 12:26:22 +0200

    HEAD and TAIL

--- src/main/java/com/ordonteam/function/playground/List.kt
+++ src/main/java/com/ordonteam/function/playground/List.kt
@@ -1,7 +1,13 @@
 package com.ordonteam.function.playground
 
 val EMPTY_LIST = F { x -> TRUE }
+
 val IS_EMPTY_LIST = F { p -> p.call(FALSE) }
+
 val PAIR = F { a -> F { b -> F { x -> x.call(a).call(b) } } }
+
 val APPEND_LIST = F { list -> F { element -> PAIR.call(element).call(list) } }
 
+val HEAD = TRUE
+
+val TAIL = FALSE
--- src/test/java/com/ordonteam/function/playground/ListTest.kt
+++ src/test/java/com/ordonteam/function/playground/ListTest.kt
@@ -4,6 +4,7 @@ import org.junit.Assert.assertEquals
 import org.junit.Test
 
 class ListTest {
+
     @Test
     fun shouldRepresentNotANumberAndNotATuple() {
         assertBehaveLikeTrue(IS_EMPTY_LIST.call(EMPTY_LIST))
@@ -13,28 +14,22 @@ class ListTest {
 
     @Test
     fun shouldRepresentRequestedPair() {
-        assertFunctionRepresent(1, PAIR.call(ONE).call(ZERO).call(TRUE))
-        assertFunctionRepresent(1, PAIR.call(ZERO).call(ONE).call(FALSE))
+        assertFunctionRepresent(1, PAIR.call(ONE).call(ZERO).call(HEAD))
+        assertFunctionRepresent(1, PAIR.call(ZERO).call(ONE).call(TAIL))
     }
 
     @Test
     fun shouldRepresentList_0_1_2() {
         val LIST = PAIR.call(ONE).call(PAIR.call(ZERO).call(TRUE.call(TWO)))
 
-        assertFunctionRepresent(1, LIST.call(TRUE))
-        assertFunctionRepresent(0, LIST.call(FALSE).call(TRUE))
-        assertFunctionRepresent(2, LIST.call(FALSE).call(FALSE).call(TRUE))
-    }
-
-    @Test
-    fun shouldRepresentEmptyList() {
-        assertEquals(EMPTY_LIST, EMPTY_LIST.call(TRUE))
-        assertEquals(EMPTY_LIST, EMPTY_LIST.call(FALSE))
+        assertFunctionRepresent(1, LIST.call(HEAD))
+        assertFunctionRepresent(0, LIST.call(TAIL).call(HEAD))
+        assertFunctionRepresent(2, LIST.call(TAIL).call(TAIL).call(HEAD))
     }
 
     @Test
     fun shouldAppendToList() {
-        assertFunctionRepresent(2, APPEND_LIST.call(EMPTY_LIST).call(TWO).call(TRUE))
-        assertEquals(EMPTY_LIST, APPEND_LIST.call(EMPTY_LIST).call(TWO).call(FALSE))
+        assertFunctionRepresent(2, APPEND_LIST.call(EMPTY_LIST).call(TWO).call(HEAD))
+        assertEquals(EMPTY_LIST, APPEND_LIST.call(EMPTY_LIST).call(TWO).call(TAIL))
     }
 }
